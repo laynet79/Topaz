10ea2f89b15029dd7b586518cfb0d9bd90fb6c15
Layne Thorup <layne@lthorup.com>
2019-01-27 12:38:53 -0700

    reset to run tokenizer test

--- Topaz/main.cpp
+++ Topaz/main.cpp
@@ -5,18 +5,18 @@ using namespace std;
 int main()
 {
 	Topaz t;
-	try
-	{
-		double n = 4.0;
-		Value* r = t.call(nullptr, "Math", "sqrt", 1, t.value(n));
-		cout << "sqrt(" << n << ")=" << t.str(r) << endl;
-	}
-	catch (char* msg)
-	{
-		cout << "ERROR: " << msg << endl;
-	}
+	//try
+	//{
+	//	double n = 4.0;
+	//	Value* r = t.call(nullptr, "Math", "sqrt", 1, t.value(n));
+	//	cout << "sqrt(" << n << ")=" << t.str(r) << endl;
+	//}
+	//catch (char* msg)
+	//{
+	//	cout << "ERROR: " << msg << endl;
+	//}
 
-	//t.runTest();
+	t.runTest();
 	system("PAUSE");
 	return 0;
 }
